04d78d3e83b5870dd2efc820690fdd9a61e581d9
Adam Drescher <adam.r.drescher@gmail.com>
2016-10-03 15:06:19 -0500

    added clock_gettime and clock_nanosleep for more precise timing.

--- statmon.cpp
+++ statmon.cpp
@@ -6,7 +6,7 @@
 // linux and C includes 
 #include <csignal>
 #include <cstdlib>
-#include <sys/time.h>
+#include <ctime>
 #include <unistd.h>
 
 // C++ includes
@@ -71,8 +71,40 @@ void setupNetlink(nl_sock *&sock, nl_cache *&link_cache)
     }
 }
 
+// taken from https://gist.github.com/BinaryPrison/1112092
+static inline uint32_t __iter_div_u64_rem(uint64_t dividend, uint32_t divisor, uint64_t *remainder)
+{
+	uint32_t ret = 0;
+	while (dividend >= divisor) {
+		/* The following asm() prevents the compiler from
+		   optimising this loop into a modulo operation.  */
+		asm("" : "+rm"(dividend));
+		dividend -= divisor;
+		ret++;
+	}
+	*remainder = dividend;
+	return ret;
+}
+
+static inline void timespec_add_ns(struct timespec *a, uint64_t ns)
+{
+	a->tv_sec += __iter_div_u64_rem(a->tv_nsec + ns, StatmonConstants::NANO, &ns);
+	a->tv_nsec = ns;
+}
+
+static inline uint64_t timespec_to_ns(struct timespec *t)
+{
+	return (StatmonConstants::NANO * t->tv_sec) + t->tv_nsec;
+}
+
+static inline uint64_t timespec_diff(struct timespec *larger, struct timespec *smaller)
+{
+	return StatmonConstants::NANO * (larger->tv_sec - smaller->tv_sec) + larger->tv_nsec - smaller->tv_nsec;
+}
+
 void collectData(const TargetVec &targets, 
-                 IfaceData &data)
+                 IfaceData &data,
+				 const uint64_t sampleRate)
 {
     int err = 0;
     nl_sock *sock;
@@ -81,8 +113,10 @@ void collectData(const TargetVec &targets,
 
     rtnl_link *link;
     InterfaceStat ifstat;
-    timeval time;
+    timespec base, loop, next;
+	uint64_t diff, offset, sleepTime;
 
+	/*
     // end - start gives us the program time without a frame of reference
 	// diff, start, and end are in seconds
     double diff, start, end;
@@ -92,14 +126,13 @@ void collectData(const TargetVec &targets,
 	// offset, base, and sleepTime are in microseconds since I am using usleep
     double offset, base, sleepTime;
     unsigned int i = 0;
+	*/
 
-    gettimeofday(&time, NULL);
-    start = time.tv_sec + (time.tv_usec / StatmonConstants::MICRO);
+    unsigned int i = 0;
+	clock_gettime(CLOCK_MONOTONIC, &base);
     while(running)
     {
-        gettimeofday(&time, NULL);
-        base = (time.tv_sec * StatmonConstants::MICRO) + time.tv_usec;
-
+        clock_gettime(CLOCK_MONOTONIC, &loop);
         err = nl_cache_resync(sock, link_cache, NULL, NULL);
         if ( unlikely(err < 0) )
         {
@@ -121,25 +154,28 @@ void collectData(const TargetVec &targets,
 
             ifstat.iter = i;
             ifstat.value = rtnl_link_get_stat(link, target.statId);
-            gettimeofday(&time, NULL);
-            end = time.tv_sec + (time.tv_usec / 1000000.0);
-            diff = end - start;
-            ifstat.timeDelta = diff;
+
+			clock_gettime(CLOCK_MONOTONIC, &next);
+            ifstat.time = timespec_diff( &next, &base);
             data[target].push_back(ifstat);
         }
 
         ++i;
-        gettimeofday(&time, NULL);
-        offset = (time.tv_sec * StatmonConstants::MICRO) + time.tv_usec;
-		sleepTime = StatmonConstants::sampleRate - (offset - base);
+		clock_gettime(CLOCK_MONOTONIC, &next);
+		sleepTime = sampleRate - timespec_diff( &next, &loop );
 		#ifdef DEBUG
 		cout << "sleeping for " << sleepTime << " microseconds." << endl;
 		#endif
 		// if sleepTime is negative, we have already taken longer than sampling rate
 		// and there is no need to sleep. No unlikely macro because it depends
 		// what the user sets the sampling rate to.
-		if (sleepTime > 0.0) 
-			usleep(sleepTime);
+		if (sleepTime > 0) 
+		{
+			timespec interval = { 0, 0 };
+			timespec_add_ns( &interval, sleepTime );
+			// try normal nanosleep too
+			clock_nanosleep(CLOCK_MONOTONIC, 0, &interval, NULL);
+		}
     }
     // terminate responsibly
     teardownNetlink(sock, link_cache);
@@ -163,57 +199,32 @@ bool parseMetric(const string &metric, rtnl_link_stat_id_t &statId)
     return valid;
 }
 
-void parseConfigFile(char *argv[], TargetVec &targets, const string &inputFile)
+void parseConfigFile(const string &inputFile, TargetVec &targets)
 {
-    string iface;
-    string metric;
+    string iface, metric;
     MeasureTarget target;
-    // we are doing file based configuration of targets, inputFile is set
-    if (inputFile != "")
-    {
-        string line;
-        ifstream in( inputFile.c_str() );
-        while (in >> iface >> metric)
-        {
-            target.iface = iface;
-            target.metric = metric;
-            targets.push_back(target);
-        }
-        in.close();
-    }
-    // it is empty, we are not doing file based and only have one target pair
-    else 
-    {
-        iface = argv[1];
-        metric = argv[2];
-
-        target.iface = iface;
-        target.metric = metric;
-        targets.push_back(target);
-    }
+	ifstream in( inputFile.c_str() );
+	while (in >> iface >> metric)
+	{
+		target.iface = iface;
+		target.metric = metric;
+		targets.push_back(target);
+	}
+	in.close();
 }
-
 
 void printUsage()
 {
-    cout << "Usage:\tstatmon iface metric rate output" << endl;
-    cout << "\tstatmon -f config rate output" << endl;
+    cout << "Usage:\tstatmon <rate> <config> <outfile> " << endl;
     cout << endl;
-    cout << "Examples:" << endl;
-    cout << "statmon eth0 rx_packets 250000 test.csv" << endl;
-    cout << "statmon -f measure.cfg 250000 test.csv" << endl; 
+	cout << "Where:" << endl;
+    cout << "    rate        the rate at which the program samples the specified metrics (in MICROseconds)" << endl;
+    cout << "    outfile     the name of the output csv file for the measurements." << endl;
+	cout << "    config      the input configuration file specifying the iface metric pairs." << endl;
+	cout << endl;
+    cout << "Example: statmon 250000 measure.cfg results.csv" << endl; 
     cout << endl;
-	cout << "Positional Arguments:" << endl;
-    cout << "    iface     which interface you want to measure." << endl;
-    cout << "    metric    which quantity you want to measure." << endl;
-    cout << "    rate      the rate at which the program samples the specified metrics (in MICROseconds)" << endl;
-    cout << "    output    the name of the output csv file for the measurements." << endl;
-    cout << endl;
-	cout << "Optional Arguments:" << endl;
-    cout << "    -f config    read in config file specifying iface metric pairs" << endl;
-    cout << "--file config" << endl;
-    cout << endl;
-	cout << "Valid metrics:" << endl;
+	cout << "Valid metrics for the configuration file:" << endl;
     cout << "    rx_packets" << endl; 
     cout << "    tx_packets" << endl; 
     cout << "    rx_bytes" << endl;
@@ -224,52 +235,47 @@ void printUsage()
     cout << "    tx_dropped" << endl;
     cout << "    rx_fifo_errors" << endl;
     cout << "    tx_fifo_errors" << endl;
+	cout << endl;
+	cout << "Example entries in configuration file:" << endl;
+	cout << endl;
+	cout << "eth0 rx_packets" << endl;
+	cout << "eth0 tx_packets" << endl;
+	cout << "eth1 rx_packets" << endl;
+	cout << endl;
+	cout << "As you can see above, each entry in the config file is space delimited and one pair per line." << endl;
+	cout << endl;
+	cout << "Important note:" << endl;
+	cout << "    Time resolution is machine dependent. You should be aware that sampling rates of too fine" << endl;
+	cout << "    granularity may not work on your machine." << endl;
+	cout << endl;
 }
-
-
-void parseArgs(int argc, char *argv[], string &inputFile)
-{
-    int ch = 0;
-    while ( (ch = getopt(argc, argv, "f:h")) != -1 )
-    {
-        switch(ch)
-        {
-        case 'f':
-            inputFile = optarg;
-            break;
-        case 'h':
-            printUsage();
-            exit(0);
-        case '?':
-        default:
-            printUsage();
-            exit(1);
-        }
-    }
-}
-
 
 int main(int argc, char *argv[])
 {
-    // no matter which way it is executed, we are expecting precisely four arguments
-    if (argc != 5)
+	// parse arguments 
+    if (argc != 4)
     {
         printUsage();
         return StatmonConstants::INVALID_NUM_ARGS;
     }
 
-    string inputFile = "";
-    parseArgs(argc, argv, inputFile);
+	uint64_t sampleRate;
 	try {
-		StatmonConstants::sampleRate = stoul( argv[3] );
+		sampleRate = stoul( argv[1] ) * 1000L;
 	} catch (...) {
 		cerr << "The argument for sample rate is not a valid unsigned integer." << endl;
 		return StatmonConstants::INVALID_SAMPLE_RATE;
 	}
-    string fname(argv[4]);
+	if ( access(argv[2], F_OK) == -1 )
+	{
+		cerr << "The provided input file does not exist." << endl;
+		return StatmonConstants::INVALID_INPUT_FILE;
+	}
+	string inputFile(argv[2]);
+    string fname(argv[3]);
 
     TargetVec targets;
-    parseConfigFile(argv, targets, inputFile);
+    parseConfigFile(inputFile, targets);
     rtnl_link_stat_id_t statId;
     for (auto &target : targets)
     {
@@ -287,12 +293,10 @@ int main(int argc, char *argv[])
     signal(SIGTERM, exitHandler);
 
     IfaceData data;
-    collectData(targets, data);
+    collectData(targets, data, sampleRate);
 
     ofstream out( fname.c_str() );
-    out.setf(ios::fixed, ios::floatfield);
-    out.precision( StatmonConstants::OUTPUT_PRECISION );
-
+	out.setf(ios::fixed);
     out << "i,time,iface,metric,value" << endl;
     MeasureTarget target;
     StatVec stats;
@@ -302,7 +306,7 @@ int main(int argc, char *argv[])
         stats = d.second;
         for (const auto &ifstat : stats)
         {
-            out << ifstat.iter << "," << ifstat.timeDelta << "," << target.iface << "," 
+            out << ifstat.iter << "," << ifstat.time << "," << target.iface << "," 
                 << target.metric << "," << ifstat.value << endl;
         }
     }
--- statmon.hpp
+++ statmon.hpp
@@ -5,30 +5,22 @@
 
 namespace StatmonConstants
 {
-    /******** general values ********/
-    const unsigned int OUTPUT_PRECISION = 2;
-
-    /* How frequently to sample the metric, given in microseconds.
-	 * Constant is no longer accurate, but it is not worth the fix. 
-	 */
-    unsigned long sampleRate = 100000;
-
-    /* Number of microseconds in one second.
-     * It is a double for division casting purposes.
-	 */
-    const double MICRO = 1000000.0;
+    /******** Time conversion constants. ********/
+    const uint64_t MICRO = 1000000L;
+	const uint64_t NANO = 1000000000L;
     /************************************/
 
     /******** main program return values ********/
     const int SUCCESS                   = 0;
     const int INVALID_NUM_ARGS          = -1;
-	const int INVALID_SAMPLE_RATE       = -2;
-    const int INVALID_METRIC            = -3;
-    const int FAILED_ALLOC_NETLINK_SOCK = -4;
-    const int FAILED_CONN_NETLINK_SOCK  = -5;
-    const int FAILED_ALLOC_LINK_CACHE   = -6;
-    const int FAILED_RESYNC_CACHE       = -7;
-    const int FAILED_GET_LINK_BY_NAME   = -8;
+	const int INVALID_INPUT_FILE        = -2;
+	const int INVALID_SAMPLE_RATE       = -3;
+    const int INVALID_METRIC            = -4;
+    const int FAILED_ALLOC_NETLINK_SOCK = -5;
+    const int FAILED_CONN_NETLINK_SOCK  = -6;
+    const int FAILED_ALLOC_LINK_CACHE   = -7;
+    const int FAILED_RESYNC_CACHE       = -8;
+    const int FAILED_GET_LINK_BY_NAME   = -9;
     /************************************/
 }
 
@@ -61,7 +53,7 @@ struct InterfaceStat
 {
     unsigned int iter;
     unsigned int value;
-    double timeDelta;
+    uint64_t time;
 };
 
 // use likely and unlikely for my error codes where I care about timing
